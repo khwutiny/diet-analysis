a460c8a1dd4d8de1e041cf6fab04037f22b86f1a
ldkbu <ldkbu@hotmail.com>
2018-06-24 13:12:49 +0800

    提交代码

--- src/components/http-common.js
+++ src/components/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-  baseURL: `http://127.0.0.1:8088`,
+  baseURL: `http://60.205.184.152:8088`,
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
