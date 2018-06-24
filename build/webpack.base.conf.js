c8791e937d573e52aa94c7c53fb86f8d846763fa
ldkbu <ldkbu@hotmail.com>
2018-06-25 00:47:34 +0800

    提交代码

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -52,7 +52,7 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
@@ -68,7 +68,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        test: /\.(woff2?|eot|svg|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
